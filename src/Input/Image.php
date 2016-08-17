41efad323ef072d37c51fbebb21115e0d652eddc
Skylar Kelty <s.kelty@kent.ac.uk>
2016-08-17 16:54:41 +0100

    Implement initial image serialize method

--- src/Input/Image.php
+++ src/Input/Image.php
@@ -12,5 +12,34 @@ class Image {
         $max = $network->getInputCount();
 
         $image = new \Imagick($filename);
+        $w = $image->getImageWidth();
+        $h = $image->getImageHeight();
+
+        while ($w * $h * 3 > $max) {
+            $tmp = $max / 3;
+            $aspect = $w / $h;
+            $w = $w - 10;
+            $h = $h - floor(10 * $a);
+        }
+
+        $image->adaptiveResizeImage($w, $h);
+
+        // Build a flattened RGB array.
+        $flattened = array();
+
+        $it = $image->getPixelIterator();
+        foreach ($it as $row => $pixels) {
+            $r[$row] = array();
+            $g[$row] = array();
+            $b[$row] = array();
+            foreach ($pixels as $column => $pixel) {
+                $rgb = $pixel->getColor();
+                $flattened[] = $rgb[0];
+                $flattened[] = $rgb[1];
+                $flattened[] = $rgb[2];
+            }
+        }
+
+        return $flattened;
     }
 }
